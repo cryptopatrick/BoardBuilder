dbc3c5ec5ccbb0dff2092cbb4ceb287d859b1ad2
cryptopatrick <cryptopatrick@gmail.com>
2023-08-05 08:21:17 +0200

    feature: add axum server
    A localhost server ping to confirm the server.

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,3 +1,18 @@
-fn main() {
-    println!("Hello, world!");
+use axum::{routing::get, Router};
+
+#[tokio::main]
+async fn main() {
+    // Adding an endpoint: http://localhost:4000/ping
+    let app = Router::new()
+        .route("/ping", get(ping));
+    
+    println!("Server listening on port 4000"); 
+    axum::Server::bind(&"127.0.0.1:4000".parse().unwrap())
+        .serve(app.into_make_service())
+        .await
+        .unwrap()
+}
+
+async fn ping() -> String {
+    "Server was pinged".to_owned()
 }
